3b397394b2595c20ab2418ce5130aa19a973eef7
JAPMelendez <jparadamuft@gmail.com>
2021-02-10 20:09:48 -0300

    Feature: add new function to the button

--- src/App/screens/DogsScreen/index.js
+++ src/App/screens/DogsScreen/index.js
@@ -1,10 +1,14 @@
 /* eslint-disable react/jsx-no-target-blank */
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Dogs.module.css";
 import axios from 'axios';
 
 const DogsScreen = () => {
   const [image, setimage] = useState('')
+  const [numbers, Setnumbers] = useState([]);
+  const [imageArray, SetImageArray] = useState([]);
+  
+
   const handleRandom = () =>{
     axios({
       method:'GET',
@@ -12,9 +16,18 @@ const DogsScreen = () => {
     })
     .then(function (response){
       setimage(response.data.message);
+      SetImageArray([...imageArray, response.data.message]);
+      Setnumbers([...numbers, {contador: numbers.length+1, url: response.data.message}]);
     })
   }
+
   
+  useEffect(()=>{
+    console.log(numbers);
+  },[numbers]
+  )
+
+
   return (
     
     <section className={styles.section}>
@@ -32,13 +45,27 @@ const DogsScreen = () => {
       <p className={styles.paragraph_considerations}>
         Considerations, the image need to be fit in the green square
       </p>
+     
       <div className={styles.content}>
         <div className={styles.img_content}>
-        <img src={image} alt="Girl in a jacket" className={styles.imgdog}/> 
+        <img src={image} alt="Perritos" className={styles.imgdog}/> 
 
         </div>
-        <button onClick = {handleRandom}  >put your random button here</button>
+        <button onClick = {handleRandom} >put your random button here</button>
       </div>
+      <div>
+        
+      </div>
+
+      esto funciona
+      {
+        numbers.map((num)=>{
+          return (<p key = {num.contador}> Perrito N°{num.contador} 
+          <img src={num.url} alt="Perritos" className={styles.imgdog}></img>
+         </p>)
+        })
+      }
+
     </section>
   );
 };
